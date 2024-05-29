d13894baa58cd473cc640c576f8beb48910c57f7
fitodac <fitodac@gmail.com>
2024-05-29 12:00:00 -0300

    phase model

--- app/Models/Phase.php
+++ app/Models/Phase.php
@@ -18,7 +18,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Phase extends Model implements HasMedia
 {
-  use HasFactory, InteractsWithMedia;
+	use HasFactory, InteractsWithMedia;
 	// use SoftDeletes;
 
 	public $timestamps = false;
@@ -62,7 +62,7 @@ class Phase extends Model implements HasMedia
 	{
 		$resp = [];
 
-		foreach($this->pics as $pic){
+		foreach ($this->pics as $pic) {
 			$img = $this->getMedia($pic);
 			$resp[$pic] = isset($img[0]) ? [
 				'id' => $img[0]->id,
@@ -84,10 +84,10 @@ class Phase extends Model implements HasMedia
 	 */
 	private function fetch_store_images($request, $phase, $type)
 	{
-		foreach( $this->pics as $pic ){
-			if( isset($request->file('phases')[$type]['images'][$pic]) ){
+		foreach ($this->pics as $pic) {
+			if (isset($request->file('phases')[$type]['images'][$pic])) {
 				$phase->addMedia($request->file('phases')[$type]['images'][$pic])
-					->setFileName( substr(md5(time()), 0, 16) .'.'. $request->file('phases')[$type]['images'][$pic]->extension() )
+					->setFileName(substr(md5(time()), 0, 16) . '.' . $request->file('phases')[$type]['images'][$pic]->extension())
 					->withResponsiveImages()
 					->toMediaCollection($pic);
 			}
@@ -103,17 +103,17 @@ class Phase extends Model implements HasMedia
 	public function store_images($request, $patient_id)
 	{
 
-		if( !empty($request['phases']['initial']) ){
+		if (!empty($request['phases']['initial'])) {
 			$initial = Phase::create(['code' => 'initial', 'name' => 'Inicial', 'patient_id' => $patient_id]);
 			$this->fetch_store_images($request, $initial, 'initial');
 		}
 
-		if( !empty($request['phases']['middle']) ){
+		if (!empty($request['phases']['middle'])) {
 			$middle = Phase::create(['code' => 'middle', 'name' => 'Media', 'patient_id' => $patient_id]);
 			$this->fetch_store_images($request, $middle, 'middle');
 		}
 
-		if( !empty($request['phases']['final']) ){
+		if (!empty($request['phases']['final'])) {
 			$final = Phase::create(['code' => 'final', 'name' => 'Final', 'patient_id' => $patient_id]);
 			$this->fetch_store_images($request, $final, 'final');
 		}
@@ -125,36 +125,32 @@ class Phase extends Model implements HasMedia
 	public function update_images($request)
 	{
 
-		foreach( $request['phases'][$this->code]['images'] as $idx => $image )
-		{
+		foreach ($request['phases'][$this->code]['images'] as $idx => $image) {
 
 			$key = array_search($idx, array_column($request['phases'][$this->code]['media'], 'collection_name'));
 
-			if($image){
-				if( isset($request->file('phases')[$this->code]['images'][$idx]) ){
-					if( is_numeric($key) ){
+			if ($image) {
+				if (isset($request->file('phases')[$this->code]['images'][$idx])) {
+					if (is_numeric($key)) {
 						$img = $this->getMedia($idx)->where('id', $request['phases'][$this->code]['media'][$key]['id']);
-						if( isset($img[0]) ) $img[0]->delete();
-						var_dump($img);
-						var_dump($key);
+						if (isset($img[0])) $img[0]->delete();
+						// var_dump($img);
+						// var_dump($key);
 					}
 
 					$this->addMedia($request->file('phases')[$this->code]['images'][$idx])
-						->setFileName( substr(md5(time()), 0, 16) .'.'. $request->file('phases')[$this->code]['images'][$idx]->extension() )
+						->setFileName(substr(md5(time()), 0, 16) . '.' . $request->file('phases')[$this->code]['images'][$idx]->extension())
 						->withResponsiveImages()
 						->toMediaCollection($idx);
 				}
+			} else {
 
-			}else{
-
-				if( is_numeric($key) ){
+				if (is_numeric($key)) {
 					$img = $this->getMedia($idx)->where('id', $request['phases'][$this->code]['media'][$key]['id']);
-					if( isset($img[0]) ) $img[0]->delete();
+					if (isset($img[0])) $img[0]->delete();
 				}
 			}
-
 		}
-
 	}
 
 
@@ -168,9 +164,9 @@ class Phase extends Model implements HasMedia
 	public function get_minies()
 	{
 		$resp = [];
-		foreach($this->pics as $pic){
+		foreach ($this->pics as $pic) {
 			$img = $this->getMedia($pic);
-			if( !empty($img[0]) ) array_push($resp, $img[0]->getUrl('minnie'));
+			if (!empty($img[0])) array_push($resp, $img[0]->getUrl('minnie'));
 		}
 		return $resp;
 	}
@@ -188,6 +184,4 @@ class Phase extends Model implements HasMedia
 		$this->addMediaConversion('minnie')->width(100)->height(100);
 		$this->addMediaConversion('thumb')->width(300)->height(300);
 	}
-
-
 }
